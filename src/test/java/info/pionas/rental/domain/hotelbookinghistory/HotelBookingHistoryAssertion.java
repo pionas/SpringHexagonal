84047a9c62376cf3c909c4c0e8202b51d76d2d9c
Adrian Pionka <adrian@pionka.com>
2021-01-20 14:25:58 +0100

    HotelBookingHistory - check create hotel booking history

---
+++ src/test/java/info/pionas/rental/domain/hotelbookinghistory/HotelBookingHistoryAssertion.java
@@ -0,0 +1,70 @@
+package info.pionas.rental.domain.hotelbookinghistory;
+
+
+import org.assertj.core.api.AbstractObjectAssert;
+import org.assertj.core.api.Assertions;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class HotelBookingHistoryAssertion {
+    private final HotelBookingHistory actual;
+
+    private HotelBookingHistoryAssertion(HotelBookingHistory actual) {
+        this.actual = actual;
+    }
+
+    public static HotelBookingHistoryAssertion assertThat(HotelBookingHistory actual) {
+        return new HotelBookingHistoryAssertion(actual);
+    }
+
+    public HotelBookingHistoryAssertion hasHotelRoomBookingHistoryFor(String hotelRoomId, LocalDateTime bookingDateTime, String tenantId, List<LocalDate> days) {
+        return hasHotelRoomBookingHistoryFor(hotelRoomBookingHistory -> {
+            HotelRoomBookingHistoryAssertion.assertThat(hotelRoomBookingHistory)
+                    .hasHotelRoomIdEqualTo(hotelRoomId)
+                    .hasHotelRoomBookingFor(bookingDateTime, tenantId, days);
+        });
+    }
+
+    public HotelBookingHistoryAssertion hasHotelRoomBookingHistoryFor(String hotelRoomId, String tenantId, List<LocalDate> days) {
+        return hasHotelRoomBookingHistoryFor(hotelRoomBookingHistory -> {
+            HotelRoomBookingHistoryAssertion.assertThat(hotelRoomBookingHistory)
+                    .hasHotelRoomIdEqualTo(hotelRoomId)
+                    .hasHotelRoomBookingFor(tenantId, days);
+        });
+    }
+
+    public HotelBookingHistoryAssertion hasInformationAboutHistoryOfHotelRoom(String hotelRoomId, int size) {
+        return hasHotelRoomBookingHistoryFor(hotelRoomBookingHistory -> {
+            HotelRoomBookingHistoryAssertion.assertThat(hotelRoomBookingHistory)
+                    .hasHotelRoomIdEqualTo(hotelRoomId)
+                    .hasInformationAboutBookings(size);
+        });
+    }
+
+    private HotelBookingHistoryAssertion hasHotelRoomBookingHistoryFor(Consumer<HotelRoomBookingHistory> consumer) {
+        hasHotelRoomBookingHistories().satisfies(actualBookings -> {
+            Assertions.assertThat(asHotelRoomHistories(actualBookings)).anySatisfy(consumer);
+        });
+
+        return this;
+    }
+
+    public HotelBookingHistoryAssertion hasInformationAboutHistoryOfHotelRooms(int size) {
+        hasHotelRoomBookingHistories().satisfies(actualBookings -> {
+            Assertions.assertThat(asHotelRoomHistories(actualBookings)).hasSize(size);
+        });
+
+        return this;
+    }
+
+    private AbstractObjectAssert<?, ?> hasHotelRoomBookingHistories() {
+        return Assertions.assertThat(actual).extracting("hotelRoomBookingHistories");
+    }
+
+    private List<HotelRoomBookingHistory> asHotelRoomHistories(Object actualBookings) {
+        return (List<HotelRoomBookingHistory>) actualBookings;
+    }
+}
--- src/test/java/info/pionas/rental/domain/hotelbookinghistory/HotelBookingHistoryTest.java
+++ src/test/java/info/pionas/rental/domain/hotelbookinghistory/HotelBookingHistoryTest.java
@@ -1,12 +1,32 @@
 package info.pionas.rental.domain.hotelbookinghistory;
 
+import com.google.common.collect.ImmutableList;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author Adi
- */
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class HotelBookingHistoryTest {
 
+    @Test
+    public void shouldCreateHotelBookingHistoryWithAllInformation() {
+        String hotelId = "123";
+        HotelBookingHistory hotelBookingHistory = new HotelBookingHistory(hotelId);
+        String hotelRoomId = "456";
+        LocalDateTime bookingDateTime = LocalDateTime.now();
+        String tenantId = "789";
+        List<LocalDate> days = ImmutableList.of(
+                LocalDate.of(2020, 5, 5),
+                LocalDate.of(2020, 5, 6),
+                LocalDate.of(2020, 5, 7)
+        );
+        hotelBookingHistory.add(hotelRoomId, bookingDateTime, tenantId, days);
+
+        HotelBookingHistoryAssertion.assertThat(hotelBookingHistory)
+                .hasInformationAboutHistoryOfHotelRooms(1)
+                .hasInformationAboutHistoryOfHotelRoom(hotelRoomId, 1)
+                .hasHotelRoomBookingHistoryFor(hotelRoomId, bookingDateTime, tenantId, days);
+
+    }
 }
