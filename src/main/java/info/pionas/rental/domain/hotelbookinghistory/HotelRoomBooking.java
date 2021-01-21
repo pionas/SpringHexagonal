e454b074d992d39bbbac7c681d457554281f2b97
Adrian Pionka <adrian@pionka.com>
2021-01-21 09:49:51 +0100

    HotelRoomBooking - create entity

--- src/main/java/info/pionas/rental/domain/hotelbookinghistory/HotelRoomBooking.java
+++ src/main/java/info/pionas/rental/domain/hotelbookinghistory/HotelRoomBooking.java
@@ -1,21 +1,37 @@
 package info.pionas.rental.domain.hotelbookinghistory;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Adi
  */
-@RequiredArgsConstructor
-//@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 class HotelRoomBooking {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private LocalDateTime bookingDateTime;
+    private String tenantId;
+    @ElementCollection
+    private List<LocalDate> days;
 
-    private final LocalDateTime bookingDateTime;
-    private final String tenantId;
-    private final List<LocalDate> days;
+    HotelRoomBooking(LocalDateTime bookingDateTime, String tenantId, List<LocalDate> days) {
+        this.bookingDateTime = bookingDateTime;
+        this.tenantId = tenantId;
+        this.days = days;
+    }
 
     public static HotelRoomBooking start(LocalDateTime bookingDateTime, String tenantId, List<LocalDate> days) {
         return new HotelRoomBooking(bookingDateTime, tenantId, days);
