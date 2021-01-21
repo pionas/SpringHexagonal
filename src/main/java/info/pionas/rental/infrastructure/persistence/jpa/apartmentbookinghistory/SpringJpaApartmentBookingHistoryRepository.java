4c843779b21f2d56c033b99168292b61a40dc89e
Adrian Pionka <adrian@pionka.com>
2021-01-21 10:56:02 +0100

    ApartmentBookingHistoryRepository - add missing tests

--- src/main/java/info/pionas/rental/infrastructure/persistence/jpa/apartmentbookinghistory/SpringJpaApartmentBookingHistoryRepository.java
+++ src/main/java/info/pionas/rental/infrastructure/persistence/jpa/apartmentbookinghistory/SpringJpaApartmentBookingHistoryRepository.java
@@ -1,9 +1,10 @@
 package info.pionas.rental.infrastructure.persistence.jpa.apartmentbookinghistory;
 
+import info.pionas.rental.domain.apartmentbookinghistory.ApartmentBookingHistory;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * @author Adi
  */
-interface SpringJpaApartmentBookingHistoryRepository
-//        extends CrudRepository<ApartmentBookingHistory, String> 
-{
+interface SpringJpaApartmentBookingHistoryRepository extends CrudRepository<ApartmentBookingHistory, String> {
 }
