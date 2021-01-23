6b374586de97ed8db8319dc4a2a0401928b7f743
Adrian Pionka <adrian@pionka.com>
2021-01-23 11:40:29 +0100

    ParameterObject for HotelApplicationService - remove @SuppressWarnings annotation

--- src/main/java/info/pionas/rental/application/hotel/HotelApplicationService.java
+++ src/main/java/info/pionas/rental/application/hotel/HotelApplicationService.java
@@ -12,7 +12,6 @@ public class HotelApplicationService {
 
     private final HotelRepository hotelRepository;
 
-    @SuppressWarnings("checkstyle:ParameterNumber")
     public String add(String name, String street, String postalCode, String buildingNumber, String city, String country) {
         Hotel hotel = new HotelFactory().create(name, street, postalCode, buildingNumber, city, country);
         return hotelRepository.save(hotel);
