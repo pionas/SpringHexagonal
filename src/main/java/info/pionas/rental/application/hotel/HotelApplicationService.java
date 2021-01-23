90c2466a8799d938b5a78df5178c1f64854d782d
Adrian Pionka <adrian@pionka.com>
2021-01-23 11:55:25 +0100

    ParameterObject for HotelApplicationService - use Hotel.Bu  instead of HotelFactory

--- src/main/java/info/pionas/rental/application/hotel/HotelApplicationService.java
+++ src/main/java/info/pionas/rental/application/hotel/HotelApplicationService.java
@@ -1,7 +1,6 @@
 package info.pionas.rental.application.hotel;
 
 import info.pionas.rental.domain.hotel.Hotel;
-import info.pionas.rental.domain.hotel.HotelFactory;
 import info.pionas.rental.domain.hotel.HotelRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,14 +12,14 @@ public class HotelApplicationService {
     private final HotelRepository hotelRepository;
 
     public String add(HotelDto hotelDto) {
-        Hotel hotel = new HotelFactory().create(
-                hotelDto.getName(),
-                hotelDto.getStreet(),
-                hotelDto.getPostalCode(),
-                hotelDto.getBuildingNumber(),
-                hotelDto.getCity(),
-                hotelDto.getCountry()
-        );
+        Hotel hotel = Hotel.Builder.hotel()
+                .withName(hotelDto.getName())
+                .withStreet(hotelDto.getStreet())
+                .withPostalCode(hotelDto.getPostalCode())
+                .withBuildingNumber(hotelDto.getBuildingNumber())
+                .withCity(hotelDto.getCity())
+                .withCountry(hotelDto.getCountry())
+                .build();
         return hotelRepository.save(hotel);
     }
 }
