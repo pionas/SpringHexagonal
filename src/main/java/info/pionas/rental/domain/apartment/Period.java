30611e624350ca4608d4268683322d18d8a0bdce
Adrian Pionka <adrian@pionka.com>
2021-01-25 13:49:27 +0100

    Period - add EqualsAndHashCode annotation

--- src/main/java/info/pionas/rental/domain/apartment/Period.java
+++ src/main/java/info/pionas/rental/domain/apartment/Period.java
@@ -1,11 +1,9 @@
 package info.pionas.rental.domain.apartment;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-import javax.persistence.Embeddable;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,7 +11,7 @@ import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Getter
-@Embeddable
+@EqualsAndHashCode
 public class Period {
 
     private final LocalDate start;
@@ -23,24 +21,4 @@ public class Period {
         return start.datesUntil(end.plusDays(1)).collect(Collectors.toList());
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Period period = (Period) o;
-
-        return new EqualsBuilder().append(start, period.start).append(end, period.end).isEquals();
-    }
-
-    @Override
-    @SuppressWarnings("checkstyle:MagicNumber")
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37).append(start).append(end).toHashCode();
-    }
 }
