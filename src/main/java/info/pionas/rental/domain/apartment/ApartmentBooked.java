a930ca6e8c9dd355ba13cb6e63f84b0bdc1742bb
Adrian Pionka <adrian@pionka.com>
2021-01-26 10:24:39 +0100

    ApartmentBooked creation - remove not used methods from ApartmentBooked

--- src/main/java/info/pionas/rental/domain/apartment/ApartmentBooked.java
+++ src/main/java/info/pionas/rental/domain/apartment/ApartmentBooked.java
@@ -18,27 +18,6 @@ public final class ApartmentBooked {
     private final LocalDate periodStart;
     private final LocalDate periodEnd;
 
-    private ApartmentBooked(String eventId, LocalDateTime eventCreationDateTime, String apartmentId, String ownerId, String tenantId, Period period) {
-        this.eventId = eventId;
-        this.eventCreationDateTime = eventCreationDateTime;
-        this.apartmentId = apartmentId;
-        this.ownerId = ownerId;
-        this.tenantId = tenantId;
-        this.periodStart = period.getStart();
-        this.periodEnd = period.getEnd();
-    }
-
-    @SuppressWarnings("checkstyle:ParameterNumber")
-    static ApartmentBooked create(String eventId, String apartmentId, String ownerId, String tenantId, Period period) {
-        LocalDateTime eventCreationDateTime = LocalDateTime.now();
-        return new ApartmentBooked(eventId, eventCreationDateTime, apartmentId, ownerId, tenantId, period);
-    }
-
-    @SuppressWarnings("checkstyle:ParameterNumber")
-    static ApartmentBooked create(String eventId, LocalDateTime eventCreationDateTime, String apartmentId, String ownerId, String tenantId, Period period) {
-        return new ApartmentBooked(eventId, eventCreationDateTime, apartmentId, ownerId, tenantId, period);
-    }
-
     public static class Builder {
         private String eventId;
         private LocalDateTime eventCreationDateTime;
