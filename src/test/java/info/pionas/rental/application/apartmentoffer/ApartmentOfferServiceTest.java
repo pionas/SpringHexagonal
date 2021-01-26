4b4e3eeae1e8873b66270813891747ba62c7d4da
Adrian Pionka <adrian@pionka.com>
2021-01-26 12:52:59 +0100

    ApartmentOffer - TDD - RED - should fail in case of no apartment

--- src/test/java/info/pionas/rental/application/apartmentoffer/ApartmentOfferServiceTest.java
+++ src/test/java/info/pionas/rental/application/apartmentoffer/ApartmentOfferServiceTest.java
@@ -1,5 +1,7 @@
 package info.pionas.rental.application.apartmentoffer;
 
+import info.pionas.rental.domain.apartment.ApartmentNotFoundException;
+import info.pionas.rental.domain.apartment.ApartmentRepository;
 import info.pionas.rental.domain.apartmentoffer.ApartmentOffer;
 import info.pionas.rental.domain.apartmentoffer.ApartmentOfferAssertion;
 import info.pionas.rental.domain.apartmentoffer.ApartmentOfferRepository;
@@ -9,6 +11,9 @@ import org.mockito.ArgumentCaptor;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
 
@@ -18,17 +23,18 @@ class ApartmentOfferServiceTest {
     private static final LocalDate START = LocalDate.of(2020, 10, 11);
     private static final LocalDate END = LocalDate.of(2020, 10, 20);
 
-    private final ApartmentOfferRepository repository = mock(ApartmentOfferRepository.class);
-    private final ApartmentOfferService service = new ApartmentOfferService(repository);
+    private final ApartmentRepository apartmentRepository = mock(ApartmentRepository.class);
+    private final ApartmentOfferRepository apartmentOfferRepository = mock(ApartmentOfferRepository.class);
+    private final ApartmentOfferService service = new ApartmentOfferService(apartmentRepository, apartmentOfferRepository);
 
     @Test
-    void shouldCreateApartmenOffer() {
+    void shouldCreateApartmenOfferForExistApartment() {
         ArgumentCaptor<ApartmentOffer> captor = ArgumentCaptor.forClass(ApartmentOffer.class);
         givenExistingApartment();
 
         service.add(APARTMENT_ID, PRICE, START, END);
 
-        then(repository).should().save(captor.capture());
+        then(apartmentOfferRepository).should().save(captor.capture());
         ApartmentOffer actual = captor.getValue();
         ApartmentOfferAssertion.assertThat(actual)
                 .hasApartmentIdEqualTo(APARTMENT_ID)
@@ -36,7 +42,19 @@ class ApartmentOfferServiceTest {
                 .hasAvailabilityEqualTo(START, END);
     }
 
-    private void givenExistingApartment() {
+    @Test
+    void shouldRecognizeApartmentDoesNotExist() {
+        givenNonExistingApartment();
 
+        ApartmentNotFoundException actual = assertThrows(ApartmentNotFoundException.class, () -> service.add(APARTMENT_ID, PRICE, START, END));
+        assertThat(actual).hasMessage("Apartment with id " + APARTMENT_ID + " does not exist");
+    }
+
+    private void givenExistingApartment() {
+        given(apartmentRepository.existById(APARTMENT_ID)).willReturn(true);
+    }
+
+    private void givenNonExistingApartment() {
+        given(apartmentRepository.existById(APARTMENT_ID)).willReturn(false);
     }
 }
