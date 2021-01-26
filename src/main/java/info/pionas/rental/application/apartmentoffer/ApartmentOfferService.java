381cffa2f706dca754909dd3bfd62be5f148692a
Adrian Pionka <adrian@pionka.com>
2021-01-26 12:35:46 +0100

    ApartmentOffer - TDD - REFACTOR - introduce builder to create an object

--- src/main/java/info/pionas/rental/application/apartmentoffer/ApartmentOfferService.java
+++ src/main/java/info/pionas/rental/application/apartmentoffer/ApartmentOfferService.java
@@ -1,9 +1,7 @@
 package info.pionas.rental.application.apartmentoffer;
 
-import info.pionas.rental.domain.apartmentoffer.ApartmentAvailability;
 import info.pionas.rental.domain.apartmentoffer.ApartmentOffer;
 import info.pionas.rental.domain.apartmentoffer.ApartmentOfferRepository;
-import info.pionas.rental.domain.apartmentoffer.Money;
 import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
@@ -14,7 +12,11 @@ class ApartmentOfferService {
     private final ApartmentOfferRepository repository;
 
     void add(String apartmentId, BigDecimal price, LocalDate start, LocalDate end) {
-        ApartmentOffer offer = new ApartmentOffer(apartmentId, new Money(price), new ApartmentAvailability(start, end));
+        ApartmentOffer offer = ApartmentOffer.Builder.apartmentOffer()
+                .withApartmentId(apartmentId)
+                .withPrice(price)
+                .withAvailability(start, end)
+                .build();
         repository.save(offer);
     }
 }
--- src/main/java/info/pionas/rental/domain/apartmentoffer/ApartmentOffer.java
+++ src/main/java/info/pionas/rental/domain/apartmentoffer/ApartmentOffer.java
@@ -2,10 +2,54 @@ package info.pionas.rental.domain.apartmentoffer;
 
 import lombok.RequiredArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 @RequiredArgsConstructor
 public
 class ApartmentOffer {
     private final String apartmentId;
     private final Money money;
     private final ApartmentAvailability availability;
+
+    public static class Builder {
+
+        private String apartmentId;
+        private BigDecimal price;
+        private LocalDate start;
+        private LocalDate end;
+
+        public static Builder apartmentOffer() {
+            return new Builder();
+        }
+
+        public Builder withApartmentId(String apartmentId) {
+            this.apartmentId = apartmentId;
+            return this;
+        }
+
+        public Builder withPrice(BigDecimal price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder withAvailability(LocalDate start, LocalDate end) {
+            this.start = start;
+            this.end = end;
+            return this;
+        }
+
+        public ApartmentOffer build() {
+            return new ApartmentOffer(apartmentId, money(), availability());
+        }
+
+        private Money money() {
+            return new Money(price);
+        }
+
+        private ApartmentAvailability availability() {
+            return new ApartmentAvailability(start, end);
+        }
+
+    }
 }
