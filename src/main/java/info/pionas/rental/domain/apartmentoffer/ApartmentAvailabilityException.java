c6811b903bf68ddb8b4f3d1b9dabb9b8ff0220ac
Adrian Pionka <adrian@pionka.com>
2021-01-26 13:20:22 +0100

    ApartmentOffer - TDD - GREEN - should recognize start is after end

--- src/main/java/info/pionas/rental/domain/apartmentoffer/ApartmentAvailabilityException.java
+++ src/main/java/info/pionas/rental/domain/apartmentoffer/ApartmentAvailabilityException.java
@@ -1,4 +1,7 @@
 package info.pionas.rental.domain.apartmentoffer;
 
 public class ApartmentAvailabilityException extends RuntimeException {
+    ApartmentAvailabilityException() {
+        super("Start date of availability is after end date");
+    }
 }
