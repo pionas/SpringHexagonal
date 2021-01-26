32aeacc80ceb5970f7573eee146ac0e452a869ff
Adrian Pionka <adrian@pionka.com>
2021-01-26 11:15:52 +0100

    ParameterObject for BookingAccepted - replace usage of BookingAccepted.create with ApartmentBooked.Builder in all tests

--- src/main/java/info/pionas/rental/domain/booking/BookingAccepted.java
+++ src/main/java/info/pionas/rental/domain/booking/BookingAccepted.java
@@ -25,4 +25,50 @@ public class BookingAccepted {
         return new BookingAccepted(eventId, eventCreationDateTime, rentalType.name(), rentalPlaceId, tenantId, days);
     }
 
+    public static class Builder {
+        private String eventId;
+        private LocalDateTime eventCreationDateTime;
+        private String rentalType;
+        private String rentalPlaceId;
+        private String tenantId;
+        private List<LocalDate> days;
+
+        public static Builder bookingAccepted() {
+            return new Builder();
+        }
+
+        public Builder withEventId(String eventId) {
+            this.eventId = eventId;
+            return this;
+        }
+
+        public Builder withEventCreationDateTime(LocalDateTime eventCreationDateTime) {
+            this.eventCreationDateTime = eventCreationDateTime;
+            return this;
+        }
+
+        public Builder withRentalType(String rentalType) {
+            this.rentalType = rentalType;
+            return this;
+        }
+
+        public Builder withRentalPlaceId(String rentalPlaceId) {
+            this.rentalPlaceId = rentalPlaceId;
+            return this;
+        }
+
+        public Builder withTenantId(String tenantId) {
+            this.tenantId = tenantId;
+            return this;
+        }
+
+        public Builder withDays(List<LocalDate> days) {
+            this.days = days;
+            return this;
+        }
+
+        public BookingAccepted build() {
+            return new BookingAccepted(eventId, eventCreationDateTime, rentalType, rentalPlaceId, tenantId, days);
+        }
+    }
 }
