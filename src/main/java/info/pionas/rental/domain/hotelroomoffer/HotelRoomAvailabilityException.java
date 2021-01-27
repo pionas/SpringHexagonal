bbbb06262e14f99b8caa71e1a976f969f4b10d73
Adrian Pionka <adrian@pionka.com>
2021-01-27 13:10:23 +0100

    HotelRoomOffer - TDD - REFACTOR - introduce named constructors to create HotelRoomAvailabilityException

--- src/main/java/info/pionas/rental/domain/hotelroomoffer/HotelRoomAvailabilityException.java
+++ src/main/java/info/pionas/rental/domain/hotelroomoffer/HotelRoomAvailabilityException.java
@@ -3,11 +3,16 @@ package info.pionas.rental.domain.hotelroomoffer;
 import java.time.LocalDate;
 
 public class HotelRoomAvailabilityException extends RuntimeException {
-    HotelRoomAvailabilityException(LocalDate start, LocalDate end) {
-        super("Start date: " + start + " of availability is after end date: " + end);
+
+    private HotelRoomAvailabilityException(String message) {
+        super(message);
     }
 
-    HotelRoomAvailabilityException(LocalDate start) {
-        super("Start date: " + start + " is past date.");
+    static RuntimeException startAfterEnd(LocalDate start, LocalDate end) {
+        return new HotelRoomAvailabilityException("Start date: " + start + " of availability is after end date: " + end);
+    }
+
+    static RuntimeException startFromPast(LocalDate start) {
+        return new HotelRoomAvailabilityException("Start date: " + start + " is past date.");
     }
 }
