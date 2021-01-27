13dbecfea4dbcb19056b0516c164730a8f5fd3f2
Adrian Pionka <adrian@pionka.com>
2021-01-27 12:14:08 +0100

    HotelRoomOffer - TDD - GREEN - should recognize HotelRoom does not exist

--- src/main/java/info/pionas/rental/domain/hotelroomoffer/HotelRoomNotFoundException.java
+++ src/main/java/info/pionas/rental/domain/hotelroomoffer/HotelRoomNotFoundException.java
@@ -1,4 +1,7 @@
 package info.pionas.rental.domain.hotelroomoffer;
 
 public class HotelRoomNotFoundException extends RuntimeException {
+    public HotelRoomNotFoundException(String hotelRoomId) {
+        super("Hotel room with id " + hotelRoomId + " does not exist");
+    }
 }
