5c148a202514855767fd497b5397cbcdecac3609
Adrian Pionka <adrian@pionka.com>
2021-01-31 16:13:36 +0100

    Address - add missing test

--- src/main/java/info/pionas/rental/domain/address/Address.java
+++ src/main/java/info/pionas/rental/domain/address/Address.java
@@ -1,6 +1,11 @@
 package info.pionas.rental.domain.address;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import javax.persistence.Embeddable;
 
@@ -8,7 +13,6 @@ import javax.persistence.Embeddable;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Embeddable
-@EqualsAndHashCode
 @SuppressWarnings("PMD.UnusedPrivateField")
 public class Address {
 
@@ -17,4 +21,37 @@ public class Address {
     private String buildingNumber;
     private String city;
     private String country;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Address address = (Address) o;
+
+        return new EqualsBuilder()
+                .append(street, address.street)
+                .append(postalCode, address.postalCode)
+                .append(buildingNumber, address.buildingNumber)
+                .append(city, address.city)
+                .append(country, address.country)
+                .isEquals();
+    }
+
+    @SuppressWarnings("checkstyle:MagicNumber")
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(street)
+                .append(postalCode)
+                .append(buildingNumber)
+                .append(city)
+                .append(country)
+                .toHashCode();
+    }
 }
