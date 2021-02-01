9acaf1dfe7a2a134a6443007a75531de2e323375
Adrian Pionka <adrian@pionka.com>
2021-02-01 11:22:37 +0100

    RentalPlaceIdentifier - add missing tests

--- src/main/java/info/pionas/rental/domain/booking/RentalPlaceIdentifier.java
+++ src/main/java/info/pionas/rental/domain/booking/RentalPlaceIdentifier.java
@@ -1,11 +1,40 @@
 package info.pionas.rental.domain.booking;
 
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 @RequiredArgsConstructor
-@EqualsAndHashCode
+@Getter
 public class RentalPlaceIdentifier {
-    private final RentalType hotelRoom;
+    private final RentalType rentalType;
     private final String rentalPlaceId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        RentalPlaceIdentifier that = (RentalPlaceIdentifier) o;
+
+        return new EqualsBuilder()
+                .append(rentalType, that.rentalType)
+                .append(rentalPlaceId, that.rentalPlaceId)
+                .isEquals();
+    }
+
+    @SuppressWarnings("checkstyle:MagicNumber")
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(rentalType)
+                .append(rentalPlaceId)
+                .toHashCode();
+    }
 }
---
+++ src/test/java/info/pionas/rental/domain/booking/RentalPlaceIdentifierTest.java
@@ -0,0 +1,35 @@
+package info.pionas.rental.domain.booking;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RentalPlaceIdentifierTest {
+
+    @Test
+    void shouldRecognizeTheSameInstanceAsTheSameAggregate() {
+        RentalPlaceIdentifier actual = givenRentalPlaceIdentifier();
+
+        Assertions.assertThat(actual.equals(actual)).isTrue();
+    }
+
+    @Test
+    void shouldRecognizeNullIsNotTheSameAsRentalPlaceIdentifier() {
+        RentalPlaceIdentifier actual = givenRentalPlaceIdentifier();
+
+        assertThat(actual.equals(null)).isFalse();
+    }
+
+    @Test
+    void shouldRecognizeTwoRentalPlaceIdentifierInstancesRepresentTheSameAggregate() {
+        RentalPlaceIdentifier toCompare = givenRentalPlaceIdentifier();
+        RentalPlaceIdentifier actual = givenRentalPlaceIdentifier();
+        assertThat(actual.equals(toCompare)).isTrue();
+        assertThat(actual.hashCode()).isEqualTo(toCompare.hashCode());
+    }
+
+    private RentalPlaceIdentifier givenRentalPlaceIdentifier() {
+        return new RentalPlaceIdentifier(RentalType.HOTEL_ROOM, "121");
+    }
+}
