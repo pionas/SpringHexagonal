cef095d9f3cc559df8769daa0e52895787a6cbcd
Adrian Pionka <adrian@pionka.com>
2021-02-01 16:06:47 +0100

    User - create entity

--- src/main/java/info/pionas/usermanagment/domain/user/Name.java
+++ src/main/java/info/pionas/usermanagment/domain/user/Name.java
@@ -1,13 +1,22 @@
 package info.pionas.usermanagment.domain.user;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-@RequiredArgsConstructor
+import javax.persistence.Embeddable;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Embeddable
+@SuppressWarnings("PMD.UnusedPrivateField")
 public class Name {
-    private final String name;
-    private final String lastName;
+    private String name;
+    private String lastName;
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/info/pionas/usermanagment/domain/user/User.java
+++ src/main/java/info/pionas/usermanagment/domain/user/User.java
@@ -1,14 +1,32 @@
 package info.pionas.usermanagment.domain.user;
 
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+import javax.persistence.*;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "APARTMENT")
 public class User {
-    private final String login;
-    private final Name name;
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String login;
+    @Embedded
+    private Name name;
+
+    public String id() {
+        if (id == null) {
+            return null;
+        }
+        return id.toString();
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -22,13 +40,19 @@ public class User {
 
         User user = (User) o;
 
-        return new EqualsBuilder().append(login, user.login).append(name, user.name).isEquals();
+        return new EqualsBuilder()
+                .append(login, user.login)
+                .append(name, user.name)
+                .isEquals();
     }
 
     @SuppressWarnings("checkstyle:MagicNumber")
     @Override
     public int hashCode() {
-        return new HashCodeBuilder(17, 37).append(login).append(name).toHashCode();
+        return new HashCodeBuilder(17, 37)
+                .append(login)
+                .append(name)
+                .toHashCode();
     }
 
 }
