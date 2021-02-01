1fdce25eb38600ccabc3c0cc5e08b23955c187b6
Adrian Pionka <adrian@pionka.com>
2021-02-01 10:34:53 +0100

    BookingDomainService - add BookingDomainService as an attribute of BookingCommandHandler

--- src/main/java/info/pionas/rental/application/booking/BookingCommandHandler.java
+++ src/main/java/info/pionas/rental/application/booking/BookingCommandHandler.java
@@ -1,6 +1,7 @@
 package info.pionas.rental.application.booking;
 
 import info.pionas.rental.domain.booking.Booking;
+import info.pionas.rental.domain.booking.BookingDomainService;
 import info.pionas.rental.domain.booking.BookingEventsPublisher;
 import info.pionas.rental.domain.booking.BookingRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,23 +11,20 @@ import org.springframework.context.event.EventListener;
 public class BookingCommandHandler {
 
     private final BookingRepository bookingRepository;
+    private final BookingDomainService bookingDomainService;
     private final BookingEventsPublisher bookingEventsPublisher;
 
     @EventListener
     public void reject(BookingReject bookingReject) {
         Booking booking = bookingRepository.findById(bookingReject.getBookingId());
         booking.reject(bookingEventsPublisher);
-
         bookingRepository.save(booking);
-
     }
 
     @EventListener
     public void accept(BookingAccept bookingAccept) {
         Booking booking = bookingRepository.findById(bookingAccept.getBookingId());
         booking.accept(bookingEventsPublisher);
-
         bookingRepository.save(booking);
-
     }
 }
