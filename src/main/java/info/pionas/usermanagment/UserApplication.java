446d4c879b066a1a046930c7fa7d8096386c41f1
Adrian Pionka <adrian@pionka.com>
2021-02-01 16:49:20 +0100

    UserApplication - add SpringBootApplication annotation

---
+++ src/main/java/info/pionas/usermanagment/UserApplication.java
@@ -0,0 +1,13 @@
+package info.pionas.usermanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
