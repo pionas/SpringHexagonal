5e2d6607caa5249afca44bdcce2e4529678d115b
Adrian Pionka <adrian@pionka.com>
2021-02-01 16:56:12 +0100

    User - add missing test - shouldReturnExistingUserWeWant

--- src/test/java/info/pionas/usermanagment/infrastructure/persistence/jpa/user/JpaUserRepositoryIntegrationTest.java
+++ src/test/java/info/pionas/usermanagment/infrastructure/persistence/jpa/user/JpaUserRepositoryIntegrationTest.java
@@ -5,11 +5,15 @@ import info.pionas.usermanagment.domain.user.User;
 import info.pionas.usermanagment.domain.user.UserAssertion;
 import info.pionas.usermanagment.domain.user.UserRepository;
 import org.assertj.core.api.Assertions;
+ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -23,6 +27,15 @@ class JpaUserRepositoryIntegrationTest {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private SpringJpaUserTestRepository springJpaUserTestRepository;
+    private final List<String> userIds = new ArrayList<>();
+
+    @AfterEach
+    void deleteApartments() {
+        springJpaUserTestRepository.deleteAll(userIds);
+    }
+
 
     @Test
     void shouldThrowExceptionWhenUserDoesNotExist() {
@@ -40,12 +53,28 @@ class JpaUserRepositoryIntegrationTest {
         UserAssertion.assertThat(findBy(userId)).represents(LOGIN, NAME, LAST_NAME);
     }
 
+    @Test
+    @Transactional
+    void shouldReturnExistingUserWeWant() {
+        User user1 = new User("john.doe", new Name("John", "Doe"));
+        givenExistingUser(user1);
+        String existingId = givenExistingUser(createUser());
+        User user2 = new User("john.doe2", new Name("John", "Doe"));
+        givenExistingUser(user2);
+        User user3 = new User("john.doe3", new Name("John", "Doe"));
+        givenExistingUser(user3);
+
+        UserAssertion.assertThat(findBy(existingId)).represents(LOGIN, NAME, LAST_NAME);
+    }
+
     private User createUser() {
         return new User(LOGIN, new Name(NAME, LAST_NAME));
     }
 
     private String givenExistingUser(User user) {
-        return userRepository.save(user);
+        String userId = userRepository.save(user);
+        userIds.add(userId);
+        return userId;
     }
 
     private User findBy(String userId) {
