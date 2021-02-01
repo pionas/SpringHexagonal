8107d69ba2867d4b5cdb386f32cf7f446a809cc9
Adrian Pionka <adrian@pionka.com>
2021-02-01 18:09:19 +0100

    User - add missing test

--- src/test/java/info/pionas/usermanagment/domain/user/UserAssertion.java
+++ src/test/java/info/pionas/usermanagment/domain/user/UserAssertion.java
@@ -3,6 +3,8 @@ package info.pionas.usermanagment.domain.user;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 
+import static info.pionas.usermanagment.domain.user.User.Builder.user;
+
 @RequiredArgsConstructor
 public class UserAssertion {
     private final User actual;
@@ -12,7 +14,10 @@ public class UserAssertion {
     }
 
     public UserAssertion represents(String login, String name, String lastName) {
-        User expected = new User(login, new Name(name, lastName));
+        Assertions.assertThat(actual).hasFieldOrPropertyWithValue("login", login);
+        Assertions.assertThat(actual).hasFieldOrPropertyWithValue("name.name", name);
+        Assertions.assertThat(actual).hasFieldOrPropertyWithValue("name.lastName", lastName);
+        User expected = user().withLogin(login).withName(name, lastName).build();
         Assertions.assertThat(actual).isEqualTo(expected);
         return this;
     }
