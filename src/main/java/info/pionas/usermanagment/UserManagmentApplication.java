fdae3c6371d408ff9ed512f703b91accaa0a1518
Adrian Pionka <adrian@pionka.com>
2021-02-01 17:16:32 +0100

    Renamed from UserApplication to UserManagmentApplication

--- src/main/java/info/pionas/usermanagment/UserManagmentApplication.java
+++ src/main/java/info/pionas/usermanagment/UserManagmentApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
-public class UserApplication {
+public class UserManagmentApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(UserManagmentApplication.class, args);
     }
 
 }
