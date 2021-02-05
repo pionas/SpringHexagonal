6711f9ea8e0ee5f2abbcc882d317885b9c911742
Adrian Pionka <adrian@pionka.com>
2021-02-05 15:48:39 +0100

    ApartmentRestControllerSystemTest - set ActiveProfiles

--- src/test/java/info/pionas/rental/infrastructure/rest/api/apartment/ApartmentRestControllerSystemTest.java
+++ src/test/java/info/pionas/rental/infrastructure/rest/api/apartment/ApartmentRestControllerSystemTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -34,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Tag("SystemTest")
+@ActiveProfiles("FakeAddressCatalogue")
 class ApartmentRestControllerSystemTest {
     private static final String OWNER_ID_1 = "1234";
     private static final String STREET_1 = "Florianska";
