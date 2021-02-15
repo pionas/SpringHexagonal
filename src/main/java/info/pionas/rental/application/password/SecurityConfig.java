a7f32524e36fdec75fa314da71c179b219c77e60
Adrian Pionka <adrian@pionka.com>
2021-02-15 15:39:23 +0100

    PasswordEncoderFactory - rename to SecurityConfig

---
+++ src/main/java/info/pionas/rental/application/password/SecurityConfig.java
@@ -0,0 +1,31 @@
+package info.pionas.rental.application.password;
+
+import lombok.extern.java.Log;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@Log
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+        http.anonymous().principal("guest").authorities("GUEST_ROLE");
+        http.cors().and().csrf().disable();
+    }
+}
