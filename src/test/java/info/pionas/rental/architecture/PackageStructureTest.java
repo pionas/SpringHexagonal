3a120f2c780676c5e337a4c59b2224c8bfb02bbc
Adrian Pionka <adrian@pionka.com>
2021-02-15 16:02:30 +0100

    PackageStructureTest - add new access classes package

--- src/test/java/info/pionas/rental/architecture/PackageStructureTest.java
+++ src/test/java/info/pionas/rental/architecture/PackageStructureTest.java
@@ -13,6 +13,8 @@ class PackageStructureTest {
     private static final String JAVA = "java..";
     private static final String APACHE_COMMONS = "org.apache.commons.lang3..";
     private static final String GOOGLE_COMMONS = "com.google.common..";
+    private static final String SPRING_SECURITY = "org.springframework.security..";
+    private static final String TENANT_COMMONS = "info.pionas.common..";
     private static final String QUERY = "..query..";
     private static final String INFRASTRUCTURE = "..infrastructure..";
 
@@ -21,14 +23,14 @@ class PackageStructureTest {
     @Test
     void domainShouldTalkOnlyWithDomain() {
         classes().that().resideInAPackage(DOMAIN)
-                .should().onlyAccessClassesThat().resideInAnyPackage(DOMAIN, JAVA, APACHE_COMMONS, GOOGLE_COMMONS)
+                .should().onlyAccessClassesThat().resideInAnyPackage(DOMAIN, JAVA, APACHE_COMMONS, GOOGLE_COMMONS, SPRING_SECURITY, TENANT_COMMONS)
                 .check(classes);
     }
 
     @Test
     void applicationShouldTalkOnlyWithApplicationAndDomain() {
         classes().that().resideInAPackage(APPLICATION)
-                .should().onlyAccessClassesThat().resideInAnyPackage(APPLICATION, DOMAIN, JAVA)
+                .should().onlyAccessClassesThat().resideInAnyPackage(APPLICATION, DOMAIN, JAVA, SPRING_SECURITY)
                 .check(classes);
     }
 
