f283ef093ab8d3625990adec22f2a54dece41b5b
Adrian Pionka <adrian@pionka.com>
2021-02-16 10:16:02 +0100

    SecurityConfig - remove unused bean

--- src/main/java/info/pionas/rental/infrastructure/security/spring/SecurityConfig.java
+++ src/main/java/info/pionas/rental/infrastructure/security/spring/SecurityConfig.java
@@ -1,15 +1,12 @@
 package info.pionas.rental.infrastructure.security.spring;
 
 import lombok.extern.java.Log;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,11 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @Log
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    @SuppressWarnings("checkstyle:MagicNumber")
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
