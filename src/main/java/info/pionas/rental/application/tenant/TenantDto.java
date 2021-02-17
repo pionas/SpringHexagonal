39b1bd86813c0d6535bb8d7ca4838dae7594d6ef
Adrian Pionka <adrian@pionka.com>
2021-02-17 20:20:57 +0100

    TenantDto - add validation constraints

--- src/main/java/info/pionas/rental/application/tenant/TenantDto.java
+++ src/main/java/info/pionas/rental/application/tenant/TenantDto.java
@@ -4,14 +4,24 @@ import info.pionas.rental.domain.tenant.NewTenantDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import static info.pionas.rental.domain.tenant.NewTenantDto.Builder.newTenantDto;
 
 @AllArgsConstructor
 @Getter
 public class TenantDto {
+
+    @SuppressWarnings("checkstyle:MagicNumber")
+    @Size(min = 3, max = 250, message = "Login size must be between 3 and 250")
     private String login;
+    @Email(message = "Email must be a well-formed address")
     private String email;
+    @NotEmpty(message = "Please provide a first name")
     private String firstName;
+    @NotEmpty(message = "Please provide a last name")
     private String lastName;
     private String password;
     private String passwordRepeat;
