e33ea4ba591432a36913ceb9d6a3bc379cbd1d14
Adrian Pionka <adrian@pionka.com>
2021-02-21 14:03:44 +0100

    TenantRestController - add PaginationLinkBuilder

---
+++ src/main/java/info/pionas/rental/query/PaginationLinkBuilder.java
@@ -0,0 +1,53 @@
+package info.pionas.rental.query;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.Link;
+
+import java.util.ArrayList;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@RequiredArgsConstructor
+public class PaginationLinkBuilder {
+    private final int pageNumber;
+    private final int totalPage;
+    private Link prevPage;
+    private Link currentPage;
+    private Link nextPage;
+
+    public void setPrevPage(PagedResultTransferObject o) {
+        if (o == null) {
+            return;
+        }
+        this.prevPage = linkTo(o).withRel("prev");
+    }
+
+    public void setCurrentPage(PagedResultTransferObject o) {
+        if (o == null) {
+            return;
+        }
+        this.currentPage = linkTo(o).withSelfRel();
+    }
+
+    public void setNextPage(PagedResultTransferObject o) {
+        if (o == null) {
+            return;
+        }
+        this.nextPage = linkTo(o).withRel("next");
+    }
+
+    public ArrayList<Link> getLinks() {
+        ArrayList<Link> links = new ArrayList<Link>();
+        if (this.prevPage != null && pageNumber > 0) {
+            links.add(this.prevPage);
+        }
+        if (this.currentPage != null) {
+            links.add(this.currentPage);
+        }
+
+        if (this.nextPage != null && pageNumber < totalPage - 1) {
+            links.add(this.nextPage);
+        }
+        return links;
+    }
+}
